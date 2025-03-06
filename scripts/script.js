de34d3678cdb945f0ebf9c54d08d40191abee566
ayesha-Siddiquah <ayeshaleo93@gmail.com>
2025-03-06 15:25:22 +0530

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
+
+btnMenu.addEventListener('click', openMenu);
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+function openMenu(){
+    body.classList.toggle('show');
+	nav.classList.add('activated');
+}
+
+const mql = window.matchMedia('(min-width: 760px)');
+
+
+mql.addListener(removeTransition);
+
+
+function removeTransition(e){
+
+	if(e.matches){
+		body.classList.remove('show');
+		nav.classList.remove('activated');
+	}
+}
+
